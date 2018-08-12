649458bccf1ab7055ccf9fa24a333b1a11294e3b
arjunarjun27 <arjunarjun27@gmail.com>
2018-08-12 12:45:44 +0530

    activity lifecycle

--- app/src/main/java/com/example/arjun27/lifecycle/AnotherActivity.java
+++ app/src/main/java/com/example/arjun27/lifecycle/AnotherActivity.java
@@ -1,25 +1,21 @@
 package com.example.arjun27.lifecycle;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "ActivityA";
-
+public class AnotherActivity extends AppCompatActivity {
+    private static final String TAG = "ActivityB";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Mylog.activityprint(TAG,"onCreate");
+        setContentView(R.layout.activity_another);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         Mylog.activityprint(TAG,"onStart");
+
     }
 
 
@@ -27,12 +23,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         Mylog.activityprint(TAG,"onResume");
+
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         Mylog.activityprint(TAG,"onPause");
+
     }
 
 
@@ -40,25 +38,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         Mylog.activityprint(TAG,"onStop");
-
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
         Mylog.activityprint(TAG,"onRestart");
+
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Mylog.activityprint(TAG,"onDestroy");
-    }
-
-    public void anotheractivity(View view) {
-
-        startActivity(new Intent(this,AnotherActivity.class));
-
 
     }
 }
--- app/src/main/java/com/example/arjun27/lifecycle/Mylog.java
+++ app/src/main/java/com/example/arjun27/lifecycle/Mylog.java
@@ -3,11 +3,10 @@ package com.example.arjun27.lifecycle;
 import android.util.Log;
 
 public class Mylog {
-    private static final String TAG = "Mylog";
 
-    public static void activityprint(String msg){
+    public static void activityprint(String TAG,String msg){
 
-        Log.d(TAG, "activity: "+msg);
+        Log.d(TAG, msg);
     }
 
 }
